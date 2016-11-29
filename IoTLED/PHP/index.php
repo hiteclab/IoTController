4deb0ebbcba3788c8dd666d43874182efe2fbd8a
Laboratorio Hipermedia de Tecnologias para la Comunicación <jdcuartasc@libertadores.edu.co>
2016-11-29 15:00:10 -0500

    Add files via upload

---
+++ IoTLED/PHP/index.php
@@ -0,0 +1,80 @@
+<?php
+
+/*******************************************************
+
+IoTLED V0.2
+Control a LED with an ESP8266 v01 from a website with PHP.
+Controle un LED con un ESP8266 v01 desde un sitio web con PHP.
+https://github.com/hiteclab/IoTController
+
+********************************************************
+ENGLISH
+********************************************************
+
+Developed by Jose David Cuartas
+Hypermedia Lab of Technologies for Communication
+University Los Libertadores
+Bogotá, Colombia, 2016.
+GPL license V3
+
+********************************************************
+ESPAÑOL
+********************************************************
+
+Desarrollado por Jose David Cuartas Correa
+Laboratorio Hipermedia de Tecnologias para Comunicación
+Fundación Universitaria Los Libertadores
+Bogotá, Colombia, 2016.
+Licencia GPL Versión 3 
+
+*******************************************************/
+
+$file = 'data.txt';
+$file2 = 'data2.txt';
+
+if($_GET['GPIO0']=="ON") {
+
+	file_put_contents($file, "ON");
+}
+
+if($_GET['GPIO0']=="OFF") {
+
+	file_put_contents($file, "OFF");
+
+}
+
+if($_GET['GPIO2']=="ON") {
+
+	file_put_contents($file2, "ON");
+}
+
+if($_GET['GPIO2']=="OFF") {
+
+	file_put_contents($file2, "OFF");
+
+}
+
+echo "Estado actual GPIO0: ";
+echo file_get_contents($file); // muestra el ultimo estado
+echo " Estado actual GPIO2: ";
+echo file_get_contents($file2); // muestra el ultimo estado
+?>
+<br>
+<br>
+Control de estado para GPIO0:
+<form method="post" action="index.php?GPIO0=ON">
+    <button type="submit">ON</button>
+</form>
+<form method="post" action="index.php?GPIO0=OFF">
+    <button type="submit">OFF</button>
+</form>
+<br>
+Control de estado para GPIO2:
+<form method="post" action="index.php?GPIO2=ON">
+    <button type="submit">ON</button>
+</form>
+<form method="post" action="index.php?GPIO2=OFF">
+    <button type="submit">OFF</button>
+</form>
+
+
